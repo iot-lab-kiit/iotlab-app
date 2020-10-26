3b577cd0fd67a55e234b6181c4c3aadb5beea165
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-10-26 23:08:27 +0530

    description fixes

--- lib/pages/descriptionPage.dart
+++ lib/pages/descriptionPage.dart
@@ -25,12 +25,13 @@ class _DescriptionPageState extends State<DescriptionPage> {
                   ),
                   image: DecorationImage(
                     fit: BoxFit.cover,
-                    image: NetworkImage('https://embed-fastly.wistia.com/deliveries/49bd387c40e2c5aada92abdf973bc46d.webp?image_crop_resized=960x540'),
+                    image: NetworkImage(
+                        'https://embed-fastly.wistia.com/deliveries/49bd387c40e2c5aada92abdf973bc46d.webp?image_crop_resized=960x540'),
                   ),
                 ),
                 child: ClipRect(
                   child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 2,sigmaY: 2),
+                    filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
                     child: Container(
                       color: Colors.black.withOpacity(0.1),
                     ),
@@ -38,143 +39,170 @@ class _DescriptionPageState extends State<DescriptionPage> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left:20,top:20),
+                padding: const EdgeInsets.only(left: 20, top: 20),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     GestureDetector(
-                      child: Icon(Icons.arrow_back, size: 30,color: Colors.white,),
-                      onTap: (){
+                      child: Icon(
+                        Icons.arrow_back,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      onTap: () {
                         Navigator.of(context).pop();
                       },
                     ),
                     SizedBox(
                       height: 140,
                     ),
-                    Text('HacktoberFest 2020', style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),),
-                    Text('By IOT Lab KIIT',style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                    ),),
+                    Text(
+                      'HacktoberFest 2020',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      'By IOT Lab KIIT',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
                   ],
                 ),
               ),
             ],
           ),
           Expanded(
-            child: Stack(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue.shade800,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30),
-                      ),
-                    ),
+            child: Stack(children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff0F304C),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
                   ),
-                  ListView(
+                ),
+              ),
+              ListView(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
                     children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Container(
-                            height: 80,
+                      Container(
+                        height: 55,
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 0, left: 30.0),
                             child: Text(
-                              'Photos', style: TextStyle(
-                              fontSize: 25,
-                              color:Colors.black,
-                            ),
+                              'Photos',
+                              style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
-                          Column(
-                            children: [
-                              Container(
-                                height: 500,
-                                decoration: BoxDecoration(
-                                  color: Colors.lightBlue.shade100,
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(30),
-                                    topRight: Radius.circular(30),
-                                  ),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left:10.0,top:15,bottom: 8),
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text(
-                                              'About the event', style: TextStyle(
-                                              fontSize: 25,
-                                              color:Colors.black,
-                                            ),
-                                            ),
-                                          ),
-                                          Container(
-                                            padding: EdgeInsets.all(4),
-                                            width: MediaQuery.of(context).size.width - 10,
-                                            // height:100,
-                                            child: Text(
-                                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum semper ligula ut iaculis. Nam egestas, mi nec euismod vulputate, lectus mauris efficitur erat, vitae pellentesque justo justo ut risus. Aliquam et orci sit amet sapien posuere finibus vitae a orci. Fusce ac tellus nec augue ',maxLines: 7,
-                                              overflow: TextOverflow.ellipsis,
-                                              softWrap: false, style: TextStyle(
-                                              fontSize: 17,
-                                              color:Colors.black,
-                                            ),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text(
-                                              'Date: dd/mm/yy', style: TextStyle(
-                                              fontSize: 20,
-                                              color:Colors.black,
-                                            ),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text(
-                                              'Venue: Online', style: TextStyle(
-                                              fontSize: 20,
-                                              color:Colors.black,
-                                            ),
-                                            ),
-                                          ),
-                                          Container(
-                                            padding: EdgeInsets.all(8),
-                                            width: MediaQuery.of(context).size.width - 10,
-                                            child: Text(
-                                              'Winners or any other information regarding the event', style: TextStyle(
-                                              fontSize: 17,
-                                              color:Colors.black,
-                                            ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                            height: 500,
+                            decoration: BoxDecoration(
+                              color: Color(0xffE6F7FB),
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(30),
+                                topRight: Radius.circular(30),
                               ),
-                            ],
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 10.0, top: 15, bottom: 8),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          'About the event',
+                                          style: TextStyle(
+                                            fontSize: 25,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.all(4),
+                                        width:
+                                            MediaQuery.of(context).size.width -
+                                                10,
+                                        // height:100,
+                                        child: Text(
+                                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam condimentum semper ligula ut iaculis. Nam egestas, mi nec euismod vulputate, lectus mauris efficitur erat, vitae pellentesque justo justo ut risus. Aliquam et orci sit amet sapien posuere finibus vitae a orci. Fusce ac tellus nec augue ',
+                                          maxLines: 7,
+                                          overflow: TextOverflow.ellipsis,
+                                          softWrap: false,
+                                          style: TextStyle(
+                                            fontSize: 17,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          'Date: dd/mm/yy',
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          'Venue: Online',
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.all(8),
+                                        width:
+                                            MediaQuery.of(context).size.width -
+                                                10,
+                                        child: Text(
+                                          'Winners or any other information regarding the event',
+                                          style: TextStyle(
+                                            fontSize: 17,
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ],
-                  )
-                ]
-            ),
+                  ),
+                ],
+              )
+            ]),
           ),
         ],
       ),
