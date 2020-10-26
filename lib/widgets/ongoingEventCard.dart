d614f1e79273a0a6ac78b461fa8a9e7f831ba6b5
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-10-26 22:54:27 +0530

    navigation

--- lib/widgets/ongoingEventCard.dart
+++ lib/widgets/ongoingEventCard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iotlabapp/pages/descriptionPage.dart';
 
 onGoingEventCard(context, {String cardTitle, String imgName}) {
   return Padding(
@@ -6,36 +7,42 @@ onGoingEventCard(context, {String cardTitle, String imgName}) {
     child: Container(
       height: 150,
       width: 340,
-      child: Card(
-        semanticContainer: true,
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/$imgName'),
-              fit: BoxFit.fill,
+      child: InkWell(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => DescriptionPage()),
+        ),
+        child: Card(
+          semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/$imgName'),
+                fit: BoxFit.fill,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 10.0,
+              ),
+              child: Text(
+                cardTitle,
+                style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontFamily: 'Signatra'),
+                textAlign: TextAlign.center,
+              ),
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.only(
-              top: 10.0,
-            ),
-            child: Text(
-              cardTitle,
-              style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: 'Signatra'),
-              textAlign: TextAlign.center,
-            ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(23.0),
           ),
+          elevation: 5,
+          margin: EdgeInsets.all(10),
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(23.0),
-        ),
-        elevation: 5,
-        margin: EdgeInsets.all(10),
       ),
     ),
   );
