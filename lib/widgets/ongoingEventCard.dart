572281e572f94e6f8d6c98d683f44124c3242481
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-10-26 22:34:05 +0530

    ongoing events

--- lib/widgets/ongoingEventCard.dart
+++ lib/widgets/ongoingEventCard.dart
@@ -4,32 +4,35 @@ onGoingEventCard(context, {String cardTitle, String imgName}) {
   return Padding(
     padding: EdgeInsets.only(top: 40, left: 15, right: 15, bottom: 5),
     child: Container(
-      height: 250,
+      height: 150,
+      width: 340,
       child: Card(
-        child: Stack(
-          children: <Widget>[
-            Image.asset(
-              'assets/images/$imgName',
-              fit: BoxFit.cover,
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/$imgName'),
+              fit: BoxFit.fill,
             ),
-            Padding(
-              padding: const EdgeInsets.all(4),
-              child: Container(
-                child: Text(
-                  cardTitle,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: 'Signatra'),
-                  textAlign: TextAlign.center,
-                ),
-              ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 10.0,
             ),
-          ],
+            child: Text(
+              cardTitle,
+              style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Signatra'),
+              textAlign: TextAlign.center,
+            ),
+          ),
         ),
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10.0),
+          borderRadius: BorderRadius.circular(23.0),
         ),
         elevation: 5,
         margin: EdgeInsets.all(10),
@@ -37,3 +40,26 @@ onGoingEventCard(context, {String cardTitle, String imgName}) {
     ),
   );
 }
+
+// Stack(
+//           children: <Widget>[
+//             Image.asset(
+//               'assets/images/$imgName',
+//               fit: BoxFit.cover,
+//             ),
+//             Padding(
+//               padding: const EdgeInsets.only(left: 10, top: 8),
+//               child: Container(
+//                 child: Text(
+//                   cardTitle,
+//                   style: TextStyle(
+//                       fontSize: 30,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.white,
+//                       fontFamily: 'Signatra'),
+//                   textAlign: TextAlign.center,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
