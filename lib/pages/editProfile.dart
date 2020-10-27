5f1ebf6025bcfe498fd037729c02712a0aceb7f4
Rohan Roy Chowdhury <rohanroy2309@gmail.com>
2020-10-27 21:43:55 +0530

    update

--- lib/pages/editProfile.dart
+++ lib/pages/editProfile.dart
@@ -12,6 +12,7 @@ class _EditProfilePageState extends State<EditProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: header(context, titleText: "Edit Profile"),
       body: Column(
         children: [
@@ -30,6 +31,88 @@ class _EditProfilePageState extends State<EditProfilePage> {
             onPressed: () {},
             child: Text(
               'Change profile picture',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            minWidth: 100,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(
+                12.0,
+              ),
+              side: BorderSide(
+                color: Colors.black,
+              ),
+            ),
+            color: Colors.white,
+          ),
+          SizedBox(height: 30),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Text(
+                  'Name',
+                  style: TextStyle(fontSize: 18),
+                ),
+              )),
+          ListTile(
+            title: new TextField(
+              decoration: new InputDecoration(
+                hintText: "Rohan Roy Chowdhury",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Text(
+                  'Position',
+                  style: TextStyle(fontSize: 18),
+                ),
+              )),
+          ListTile(
+            title: new TextField(
+              decoration: new InputDecoration(
+                hintText: "Flutter Developer",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Email ID',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+          ListTile(
+            title: new TextField(
+              decoration: new InputDecoration(
+                hintText: "rohanroy2309@gmail.com",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ListTile(
+            leading: const Icon(Icons.arrow_forward_ios_rounded),
+            title: const Text('Change Password'),
+          ),
+          MaterialButton(
+            onPressed: () {},
+            child: Text(
+              'Save',
               style: TextStyle(
                 color: Colors.black,
               ),
